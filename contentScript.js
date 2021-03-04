af42a384dfa701c3671d284cef527acae5955757
BuggemDeveloper <66670928+BuggemDeveloper@users.noreply.github.com>
2021-03-04 17:51:56 +1100

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
 var element = document.getElementById("a1");
 element.parentElement.removeChild(element);
 var thx = document.getElementById("a");
-thx.innerHTML = "<p style='font-family: Ubuntu; text-align: center; font-size: 30pt;'>Ads blocked by</br><strong>FREE AD BLOCKER BY MAX PARRY</strong></p>"
+thx.innerHTML = "<p style='font-family: Ubuntu; text-align: center; font-size: 30pt;'>Ads blocked by</br><strong>FREE AD BLOCKER BY BUGGEMDEVELOPER ON GITHUB</strong></p>"
